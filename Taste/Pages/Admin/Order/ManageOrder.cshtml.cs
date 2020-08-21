3955fa290f646891a036b387a27e2d2d83605e16
Eledhrim <caner@vivaldi.net>
2020-08-21 14:21:24 +0300

    Manage Order & Order List completed

---
+++ Taste/Pages/Admin/Order/ManageOrder.cshtml.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Stripe;
+using Taste.DataAccess.Data.Repository.IRepository;
+using Taste.Models;
+using Taste.Models.ViewModels;
+using Taste.Utility;
+
+namespace Taste
+{
+    public class ManageOrderModel : PageModel
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public ManageOrderModel(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        [BindProperty]
+        public List<OrderDetailsVM> orderListVM { get; set; }
+
+        public void OnGet()
+        {
+            orderListVM = new List<OrderDetailsVM>();
+
+            List<OrderHeader> orderHeaderList = _unitOfWork.OrderHeader.GetAll(o => o.Status == SD.StatusSubmitted || o.Status == SD.StatusInProcess).OrderByDescending(u => u.PickUpTime).ToList();
+
+            foreach (OrderHeader item in orderHeaderList)
+            {
+                OrderDetailsVM individual = new OrderDetailsVM
+                {
+                    OrderHeader = item,
+                    OrderDetails = _unitOfWork.OrderDetails.GetAll(p => p.OrderId == item.Id).ToList()
+                };
+
+                orderListVM.Add(individual);
+            }
+
+        }
+
+        public IActionResult OnPostOrderPrepare(int orderID)
+        {
+            OrderHeader orderHeader = _unitOfWork.OrderHeader.GetFirstOrDefault(o => o.Id == orderID);
+
+            orderHeader.Status = SD.StatusInProcess;
+
+            _unitOfWork.Save();
+
+            return RedirectToPage("ManageOrder");
+        }
+
+        public IActionResult OnPostOrderReady(int orderID)
+        {
+            OrderHeader orderHeader = _unitOfWork.OrderHeader.GetFirstOrDefault(o => o.Id == orderID);
+
+            orderHeader.Status = SD.StatusReady;
+
+            _unitOfWork.Save();
+
+            return RedirectToPage("ManageOrder");
+        }
+
+        public IActionResult OnPostOrderCancel(int orderID)
+        {
+            OrderHeader orderHeader = _unitOfWork.OrderHeader.GetFirstOrDefault(o => o.Id == orderID);
+
+            orderHeader.Status = SD.StatusCancelled;
+
+            _unitOfWork.Save();
+
+            return RedirectToPage("ManageOrder");
+        }
+
+        public IActionResult OnPostOrderRefund(int orderID)
+        {
+            OrderHeader orderHeader = _unitOfWork.OrderHeader.GetFirstOrDefault(o => o.Id == orderID);
+
+            //refund amount
+            var options = new RefundCreateOptions
+            {
+                Amount = Convert.ToInt32(orderHeader.OrderTotal*100),
+                Reason = RefundReasons.RequestedByCustomer,
+                Charge=orderHeader.TransactionId
+            };
+            var service = new RefundService();
+            Refund refund = service.Create(options);
+
+            orderHeader.Status = SD.StatusRefunded;
+
+            _unitOfWork.Save();
+
+            return RedirectToPage("ManageOrder");
+        }
+    }
+}
--- Taste/Pages/Admin/Order/OrderDetails.cshtml.cs
+++ Taste/Pages/Admin/Order/OrderDetails.cshtml.cs
@@ -4,8 +4,11 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Stripe;
 using Taste.DataAccess.Data.Repository.IRepository;
+using Taste.Models;
 using Taste.Models.ViewModels;
+using Taste.Utility;
 
 namespace Taste
 {
@@ -30,6 +33,49 @@ namespace Taste
             };
 
             OrderDetailsVM.OrderHeader.ApplicationUser = _unitOfWork.ApplicationUser.GetFirstOrDefault(u => u.Id == OrderDetailsVM.OrderHeader.UserId);
+        }
+
+        public IActionResult OnPostOrderConfirm(int orderID)
+        {
+            OrderHeader orderHeader = _unitOfWork.OrderHeader.GetFirstOrDefault(o => o.Id == orderID);
+
+            orderHeader.Status = SD.StatusCompleted;
+
+            _unitOfWork.Save();
+
+            return RedirectToPage("OrderList");
+        }
+
+        public IActionResult OnPostOrderCancel(int orderID)
+        {
+            OrderHeader orderHeader = _unitOfWork.OrderHeader.GetFirstOrDefault(o => o.Id == orderID);
+
+            orderHeader.Status = SD.StatusCancelled;
+
+            _unitOfWork.Save();
+
+            return RedirectToPage("OrderList");
+        }
+
+        public IActionResult OnPostOrderRefund(int orderID)
+        {
+            OrderHeader orderHeader = _unitOfWork.OrderHeader.GetFirstOrDefault(o => o.Id == orderID);
+
+            //refund amount
+            var options = new RefundCreateOptions
+            {
+                Amount = Convert.ToInt32(orderHeader.OrderTotal * 100),
+                Reason = RefundReasons.RequestedByCustomer,
+                Charge = orderHeader.TransactionId
+            };
+            var service = new RefundService();
+            Refund refund = service.Create(options);
+
+            orderHeader.Status = SD.StatusRefunded;
+
+            _unitOfWork.Save();
+
+            return RedirectToPage("OrderList");
         }
     }
 }
